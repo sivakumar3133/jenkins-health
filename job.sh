43c020ae398a7b06599987ff04fe8f3b1a83dd87
siva <siva@gmail.com>
2023-09-08 07:09:34 +0000

    second

---
+++ job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo'disk space'
+diskspace=$(df-h)
+echo -e "\n=2"
+du
+echo -e "\n=2"
+echo'system Running time'
+echo -e "\n=2"
+uptime
+echo -e "\n=2"
+echo'list of files'
+echo -e "\n=2"
+ls
+echo -e "\n=2"
+echo'calender'
+echo -e "\n=2"
+cal
